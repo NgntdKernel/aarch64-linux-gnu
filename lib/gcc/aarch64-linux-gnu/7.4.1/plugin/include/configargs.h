49e7c8012fd6e861ecdc1ce5dc0749ab44d65e1a
Ahmad Thoriq Najahi <tlogitechnjhi@gmail.com>
2018-12-21 22:03:07 +0700

    Linaro GCC 7.3-20181213~Dev
    Signed-off-by: Ahmad Thoriq Najahi <tlogitechnjhi@gmail.com>

---
+++ lib/gcc/aarch64-linux-gnu/7.4.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --disable-multilib --disable-werror --enable-languages=c --target=aarch64-linux-gnu --prefix=/root/build/aarch64-linux-gnu";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
